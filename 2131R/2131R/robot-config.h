2ff66039f73048fc432cc3a37c7b38220475236c
ellisonscheuller <44121210+ellisonscheuller@users.noreply.github.com>
2018-11-11 17:08:49 -0700

    Yup

--- 2131R/2131R/robot-config.h
+++ 2131R/2131R/robot-config.h
@@ -3,8 +3,14 @@ vex::motor LeftFrontMotor = vex::motor(vex::PORT17);
 vex::motor RightFrontMotor = vex::motor(vex::PORT14);
 vex::motor LeftBackMotor = vex::motor(vex::PORT10);
 vex::motor RightBackMotor = vex::motor(vex::PORT1);
-vex::motor JawMotor = vex::motor(vex::PORT19);
+vex::motor JawMotor = vex::motor(vex::PORT16, vex::gearSetting::ratio18_1, true);
 vex::motor ArmMotor = vex::motor(vex::PORT12);
 vex::controller Ellisons_Controller = vex::controller();
-vex::motor LiftMotor1 = vex::motor(vex::PORT18);
+vex::motor LiftMotor1 = vex::motor(vex::PORT20);
 vex::motor LiftMotor2 = vex::motor(vex::PORT11);
+
+bool JawIntakePressed;
+bool JawIntakeInverse = true;
+
+bool ArmIntakePressed;
+bool ArmIntakeInverse = true;
