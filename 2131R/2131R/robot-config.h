15a308aa84392ad8497f5136fea489c658584b6b
ellisonscheuller <44121210+ellisonscheuller@users.noreply.github.com>
2018-12-06 21:37:01 -0700

    hey

--- 2131R/2131R/robot-config.h
+++ 2131R/2131R/robot-config.h
@@ -1,16 +1,19 @@
 vex::brain Brain;
 vex::motor LeftFrontMotor = vex::motor(vex::PORT17);
-vex::motor RightFrontMotor = vex::motor(vex::PORT14);
-vex::motor LeftBackMotor = vex::motor(vex::PORT10);
-vex::motor RightBackMotor = vex::motor(vex::PORT1);
-vex::motor JawMotor = vex::motor(vex::PORT16, vex::gearSetting::ratio18_1, true);
-vex::motor ArmMotor = vex::motor(vex::PORT12);
+vex::motor RightFrontMotor = vex::motor(vex::PORT18);
+vex::motor LeftBackMotor = vex::motor(vex::PORT20);
+vex::motor RightBackMotor = vex::motor(vex::PORT19);
 vex::controller Ellisons_Controller = vex::controller();
-vex::motor LiftMotor1 = vex::motor(vex::PORT20);
-vex::motor LiftMotor2 = vex::motor(vex::PORT11);
+vex::motor FeedMotor = vex::motor(vex::PORT9, vex::gearSetting::ratio18_1, 1);
+vex::motor IndexMotor = vex::motor(vex::PORT10, vex::gearSetting::ratio18_1, 0);
+vex::motor FlyWheelMotor = vex::motor(vex::PORT6);
+vex::motor FlipperMotor = vex::motor(vex::PORT16, vex::gearSetting::ratio36_1, 0);
 
-bool JawIntakePressed;
-bool JawIntakeInverse = true;
+bool FlyWheelPressed = false;
+bool FlyWheelInverse = false;
 
-bool ArmIntakePressed;
-bool ArmIntakeInverse = true;
+bool FlipperBtnPressed = false;
+bool FlipperInverted = false;
+
+bool DescorerBtnPressed = false;
+bool DescorerInverted = false;
